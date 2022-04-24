86da461c4c6b517999c2ee4112f9e49cccf31351
Hemachandran <hemavikic@gmail.com>
2022-04-24 12:20:54 +0530

    updated signature backend

--- api/signature/signature.service.js
+++ api/signature/signature.service.js
@@ -1,51 +1,37 @@
-const { create, getImages, getImageById } = require("./signature.service");
+const pool = require("../../config/database");
 
 module.exports = {
-  createImage: (req, res) => {
-    const body = req.body;
-    create(body, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({
-          success: 0,
-          message: "Database connection errror",
-        });
+  create: (data, callBack) => {
+    pool.query(
+      `insert into signatures(id, imageURL) values(?,?)`,
+      [data.id, data.imageURL],
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
+        return callBack(null, results);
       }
-      return res.status(200).json({
-        success: 1,
-        data: results,
-      });
+    );
+  },
+  getImages: (callBack) => {
+    pool.query(`select * from signatures`, [], (error, results, fields) => {
+      if (error) {
+        console.log(error);
+        return callBack(error);
+      }
+      return callBack(null, results);
     });
   },
-  getImages: (req, res) => {
-    getImages((err, results) => {
-      if (err) {
-        console.log(err);
-        return;
+  getImageById: (id, callBack) => {
+    pool.query(
+      `select * from signatures where id = ?`,
+      [id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
       }
-      return res.json({
-        success: 1,
-        data: results[0].imageURL.toString("utf8"),
-      });
-    });
-  },
-  getImageById: (req, res) => {
-    const id = req.params.id;
-    getImageById(id, (err, results) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      if (!results) {
-        return res.json({
-          success: 0,
-          message: "Record not Found",
-        });
-      }
-      return res.json({
-        success: 1,
-        data: results,
-      });
-    });
+    );
   },
 };
