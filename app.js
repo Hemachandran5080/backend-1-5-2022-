ced220cc2a7dde3945637f4f6a1d1dfce57e896f
Hemachandran <hemavikic@gmail.com>
2022-04-24 12:11:04 +0530

    Added Signature Backend

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const clientRouter = require("./api/clients/client.router");
 const jobRouter = require("./api/jobs/job.router");
 const assetRouter = require("./api/assets/asset.router");
 const commentRouter = require("./api/comments/comment.router");
+const signatureRouter = require("./api/signature/signature.router");
 var cors = require("cors");
 const PORT = 3010;
 
@@ -21,6 +22,8 @@ app.use("/api/jobs", jobRouter);
 app.use("/api/assets", assetRouter);
 
 app.use("/api/comments", commentRouter);
+
+app.use("/api/signature", signatureRouter);
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
